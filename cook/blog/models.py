eb285cb7d0695c69e2e31760793c6661d46029de
Coffe-Lake <felixkot00@gmail.com>
2021-07-17 20:07:24 +0300

    Test branch

--- cook/blog/models.py
+++ cook/blog/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
         verbose_name_plural = "категории"
 
     def __str__(self):
-        return f'{self.name[:40]}...'
+        return f"{self.name[:40]}..."
     
     
 class Tag(models.Model):
