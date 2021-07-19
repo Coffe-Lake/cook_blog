f2df4d59cfe11f84ba5e2a673ba6c32b547b27c0
Coffe-Lake <felixkot00@gmail.com>
2021-07-20 00:12:53 +0300

    Update models and admin

--- cook/blog/admin.py
+++ cook/blog/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
-from .models import Tag, Category
-from mptt.models import MPTTModel
+from mptt.admin import MPTTModelAdmin
+
+
+from . import models
+
+
+class RecipeInline(admin.StackedInline):
+    model = models.Recipe
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "author", "create_at", "id"]
+    inlines = [RecipeInline]
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', "prep_time", "cook_time", "post"]
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', "email", "post"]
+
 
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Tag)
+admin.site.register(models.Category, MPTTModelAdmin)
+admin.site.register(models.Tag)
--- cook/blog/models.py
+++ cook/blog/models.py
@@ -19,7 +19,7 @@ class Category(MPTTModel):
         order_insertion_by = ['name']
         
     class Meta:
-        verbose_name = "категории"
+        verbose_name = "категория"
         verbose_name_plural = "категории"
     
     def __str__(self):
@@ -50,7 +50,7 @@ class Post(models.Model):
         verbose_name="Автор",
         related_name="posts", 
         on_delete=models.SET_NULL,
-        null=True
+        null=True,
     )
     title = models.CharField(verbose_name="Название", max_length=200)
     image = models.ImageField(verbose_name="Изображение", upload_to='articles/')
@@ -60,23 +60,62 @@ class Post(models.Model):
         verbose_name="Категория", 
         related_name="post", 
         on_delete=models.SET_NULL, 
-        null=True
+        null=True,
     )
     tags = models.ManyToManyField(Tag, verbose_name="Теги", related_name="post")
     create_at = models.DateTimeField(verbose_name="Дата", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "пост"
+        verbose_name_plural = "посты"
+        
+    def __str__(self):
+        if len(self.title) > 15:
+            return f"{self.title[:15]}..."
+        else:
+            return self.title
+        
     
-    
-class  Recipe(models.Model):
-    name = models.CharField(max_length=100)
-    service = models.CharField(max_length=50)
-    prep_time = models.PositiveIntegerField(default=0)
-    cook_time = models.PositiveIntegerField(default=0)
-    ingredients = models.TextField()
-    directions = models.TextField()
+class Recipe(models.Model):
+    name = models.CharField(verbose_name="Имя", max_length=100)
+    service = models.CharField(verbose_name="Затрачиваемое время", max_length=50)
+    prep_time = models.PositiveIntegerField(verbose_name="Время подготовки", default=0)
+    cook_time = models.PositiveIntegerField(verbose_name="Время приготовления", default=0)
+    ingredients = models.TextField(verbose_name="Ингредиенты")
+    directions = models.TextField(verbose_name="Руководство по приготовлению")
     post = models.ForeignKey(
         Post, 
         related_name="recipe",
+        verbose_name="Пост",
         on_delete=models.SET_NULL,
         null=True,
         blank=True
     )
+    
+    class Meta:
+        verbose_name = "рецепт"
+        verbose_name_plural = "рецепты"
+        
+    def __str__(self):
+        if len(self.name) > 15:
+            return f"{self.name[:15]}..."
+        else:
+            return self.name
+    
+    
+class Comment(models.Model):
+    name = models.CharField(verbose_name="Имя", max_length=50)
+    email  = models.CharField(max_length=100)
+    website = models.CharField(verbose_name="Веб-сайт", max_length=150, blank=True)
+    message = models.TextField(verbose_name="Комментарий", max_length=500)
+    post = models.ForeignKey(Post, related_name="comment", verbose_name="Пост", on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "комментарий"
+        verbose_name_plural = "комментарии"
+    
+    def __str__(self):
+        if len(self.name) > 15:
+            return f"{self.name[:15]}..."
+        else:
+            return self.name
